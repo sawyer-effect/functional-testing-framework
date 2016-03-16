05bf2f6e6a7181d870842a8a81922bee39d8874e
Nadia Paola Garcia <nadia@sawyereffect.com>
2016-03-16 08:54:20 -0400

    Catching WebDriverException instead of a generic exception. Printing stack trace.

--- src/main/java/com/sawyereffect/steps/Hooks.java
+++ src/main/java/com/sawyereffect/steps/Hooks.java
@@ -9,6 +9,7 @@ import cucumber.api.java.BeforeStep;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,8 +38,9 @@ public class Hooks {
             try {
                 final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
                 scenario.embed(screenshot, "image/png");
-            } catch (Exception e){
-                logger.error("Not able to take screenshot");
+            } catch (WebDriverException e){
+                logger.error("Not able to take screenshot: {}", e.getMessage());
+                e.printStackTrace();
             }
         }
 
